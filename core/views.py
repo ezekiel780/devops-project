601bfc0fb789641c9277d9958f518b2b9a015316
root <root@Lipschitz.localdomain>
2026-05-07 00:58:16 +0100

    Update homepage message

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello from my DevOps Project! 🚀 Running on Kubernetes!")
+    return HttpResponse("Hello from my DevOps Project v2! 🚀 GitOps is working!")
